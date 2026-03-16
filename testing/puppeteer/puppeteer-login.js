8d50be297ce67adb377b5ed207b90dae442e339a
ethandc21 <eterpdelcerp@gmail.com>
2026-03-16 04:19:00 -0400

    Update puppeteer-login.js

--- testing/puppeteer/puppeteer-login.js
+++ testing/puppeteer/puppeteer-login.js
@@ -1,3 +1,5 @@
+// to test, make sure server is running, open a second terminal, go to approprite file path, then do 
+// node testing/puppeteer/puppeteer-login.js
 const puppeteer = require("puppeteer");
 
 (async () => {
