d0f76166528a8c4522622405af52ffe3175a8a3f
ethandc21 <eterpdelcerp@gmail.com>
2026-03-16 04:18:36 -0400

    Update puppeteer-guest.js

--- testing/puppeteer/puppeteer-guest.js
+++ testing/puppeteer/puppeteer-guest.js
@@ -1,5 +1,5 @@
-# to test, make sure server is running, open a second terminal, go to approprite file path, then do 
-# node testing/puppeteer/puppeteer-guest.js
+// to test, make sure server is running, open a second terminal, go to approprite file path, then do 
+// node testing/puppeteer/puppeteer-guest.js
 const puppeteer = require("puppeteer");
 
 (async () => {
