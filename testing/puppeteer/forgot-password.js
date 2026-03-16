1e06771c24503c26e1d84c01df69d150eab3f0da
ethandc21 <eterpdelcerp@gmail.com>
2026-03-16 04:49:37 -0400

    Update forgot-password.js

--- testing/puppeteer/forgot-password.js
+++ testing/puppeteer/forgot-password.js
@@ -1,5 +1,5 @@
 // to test, make sure server is running, open a second terminal, go to approprite file path, then do 
-// node testing/puppeteer/puppeteer-forgot-password.js
+// node testing/puppeteer/forgot-password.js
 const puppeteer = require("puppeteer");
 
 (async () => {
