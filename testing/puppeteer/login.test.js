6011c7d6c69d9d1d8b1918bbb48b8c26de0966d6
ethandc21 <eterpdelcerp@gmail.com>
2026-03-16 02:20:20 -0400

    Update login.test.js

--- testing/puppeteer/login.test.js
+++ testing/puppeteer/login.test.js
@@ -1,1 +1,32 @@
+const puppeteer = require("puppeteer");
 
+(async () => {
+
+  const browser = await puppeteer.launch({
+    headless: false   // change to true later
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:3000/login.html");
+
+  // fill form
+  await page.type("#email", "test@example.com");
+  await page.type("#password", "Password123!");
+
+  await page.click("button[type=submit]");
+
+  // wait for redirect
+  await page.waitForNavigation();
+
+  const url = page.url();
+
+  if (url.includes("chat-user")) {
+    console.log("Login test passed");
+  } else {
+    console.log("Login test failed");
+  }
+
+  await browser.close();
+
+})();
