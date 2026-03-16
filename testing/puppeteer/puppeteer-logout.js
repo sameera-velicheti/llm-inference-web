c1fa262f2d854506b504f67bb61f76d8fd83cace
ethandc21 <eterpdelcerp@gmail.com>
2026-03-16 04:39:30 -0400

    Update puppeteer-logout.js
    Testing the login and then logout sequence on the website.

--- testing/puppeteer/puppeteer-logout.js
+++ testing/puppeteer/puppeteer-logout.js
@@ -6,8 +6,13 @@ const puppeteer = require("puppeteer");
 
   try {
     console.log("Starting Logout Test...");
-    await page.goto("http://localhost:3000/chat-user.html");
+    await page.goto("http://localhost:3000/login.html");
 
+    await page.type("#email", "testuser@example.com");
+    await page.type("#password", "password123");
+
+    await page.click("#submitBtn");
+    
     await page.click("#logoutBtn");
     await page.waitForNavigation({ timeout: 5000 });
 
