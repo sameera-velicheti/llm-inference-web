529ff83dffad9c561cc37658bcd66f4efc3a3822
mtb217-arch <mtb217@scarletmail.rutgers.edu>
2026-04-15 16:27:31 -0400

    Delete authView.js

--- authView.js
+++
@@ -1,7 +0,0 @@
-function successResponse(message, data = {}) {
-  return { success: true, message, ...data };
-}
-function errorResponse(message) {
-  return { success: false, error: message };
-}
-module.exports = { successResponse, errorResponse };
